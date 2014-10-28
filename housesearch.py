923657ca9c8f846da611555249bd13fb67f2b833
Carolyn Lim <carolim@sas.upenn.edu>
2014-10-28 00:18:48 -0400

    first commit

---
+++ housesearch.py
@@ -0,0 +1,5 @@
+# query all pages
+# scrape info from a page
+# store in a table: title, price, location, date of listing
+# append all to different lists, zip into dictionary?
+# how to store info? 
