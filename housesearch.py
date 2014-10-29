eae1be1adca8fde612a884e240ec7456682a457f
Carolyn Lim <carolim@sas.upenn.edu>
2014-10-29 00:12:25 -0400

    created & populated database

--- housesearch.py
+++ housesearch.py
@@ -1,6 +1,51 @@
 import requests
-from bs4 import BeautifulSoup, NavigableString
 import sqlite3
+from bs4 import BeautifulSoup, NavigableString
+
+# creates & populates table to hold info within a given database
+def create_houses_table(dbname, houselist):
+	try:
+		db = sqlite3.connect(dbname)
+		print "Successfully connected to database..."
+		cursor = db.cursor()
+		# create table for houses
+		cursor.execute('''
+			CREATE TABLE houses(title TEXT, price INTEGER,
+				location TEXT, listingdate TEXT)
+			''')
+		print "Finished creating table..."
+		db.commit()
+
+		# insert list of tuples passed in
+		cursor.executemany('''INSERT INTO houses(title, price, location, listingdate)
+							VALUES(?,?,?,?)''', houselist)
+		print "Finished inserting into table..."
+		db.commit()
+	except Exception as e:
+		db.rollback()
+		raise e
+	finally:
+		db.close()
+
+
+# queries first 5 pages of craigslist
+def get_all_houses(url):
+	req = requests.get(url) # make a request
+	soup = BeautifulSoup(req.text)
+	houses = [] # list of tuples to be returned
+	for i in range(5):
+		params = {'s': str(i*100)}
+		req = requests.get(url, params=params)
+		soup = BeautifulSoup(req.text)
+		titles = get_house_titles(soup)
+		prices = get_house_prices(soup)
+		locations = get_house_locations(soup)
+		listingdate = get_house_listingdate(soup)
+
+		#zip into tuple
+		l = zip(titles, prices, locations, listingdate)
+		houses.extend(l)
+	return houses
 
 
 # grabs all titles of houses on page
@@ -17,8 +62,8 @@ def get_house_prices(soup):
 	page_prices = soup.find_all(class_="price")
 	prices = []
 	for price in page_prices:
-		string = price.contents[0]
-		prices.append(unicode(string))
+		string = price.contents[0].lstrip("$")
+		prices.append(int(string))
 	return prices
 
 # grabs all locations of houses on page
@@ -32,7 +77,7 @@ def get_house_locations(soup):
 
 
 # grabs date of all listings on page
-# TODO: convert to date object!!!
+# TODO: convert to date object?
 def get_house_listingdate(soup):
 	page_listdate = soup.find_all(class_="pl")
 	listdate = []
@@ -44,9 +89,8 @@ def get_house_listingdate(soup):
 
 def main():
 	url = 'http://philadelphia.craigslist.org/search/apa?bedrooms=6'
-	req = requests.get(url)
-	soup = BeautifulSoup(req.text)
-	print get_house_titles(soup)
+	houselist = get_all_houses(url)
+	create_houses_table('houseinfo.db', houselist)
 
 
 if __name__ == '__main__':
